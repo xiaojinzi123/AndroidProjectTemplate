b6caf785398ea9e93f375c392932e39e672028ce
xiaojiniz <347837667@qq.com>
2025-09-11 23:06:23 +0800

    feat: add initial Gradle configuration and ProGuard rules for Android library

---
+++ lib-res/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.xiaojinzi.lib.res"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // androidx 的 annotation
+    api(libs.kotlin.parcelize.runtime)
+    api(libs.androidx.annotation)
+    api(libs.xiaojinzi.android.support.annotation)
+    api(libs.xiaojinzi.android.support.bean)
+    api(libs.xiaojinzi.android.support.ktx)
+    api(libs.compose.runtime)
+    api(libs.compose.runtime.android)
+    api(libs.compose.ui.android)
+    api(libs.compose.foundation.android)
+    api(libs.compose.foundation.layout.android)
+    api(libs.compose.material.android)
+    api(libs.compose.material3)
+}
