1a5a4b53502071c7ac94e17ab7250abb5d7ed6d1
xiaojiniz <347837667@qq.com>
2025-09-21 10:42:56 +0800

    feat: implement Scaffold and Appbar for improved UI structure in MainActivity

--- app/src/main/java/com/xiaojinzi/template/MainActivity.kt
+++ app/src/main/java/com/xiaojinzi/template/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,7 +24,9 @@ import com.xiaojinzi.lib.res.ui.APP_PADDING_NORMAL
 import com.xiaojinzi.lib.res.ui.AppHeightSpace
 import com.xiaojinzi.lib.res.ui.theme.ProjectTemplateTheme
 import com.xiaojinzi.module.base.AppRouterConfig
+import com.xiaojinzi.module.base.view.compose.AppbarNormalM3
 import com.xiaojinzi.support.ktx.nothing
+import com.xiaojinzi.support.ktx.toStringItemDto
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,46 +34,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectTemplateTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .statusBarsPadding()
-                        .padding(horizontal = APP_PADDING_NORMAL.dp, vertical = 0.dp)
-                        .nothing(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    AppHeightSpace()
-                    AppHeightSpace()
-                    AppHeightSpace()
-                    Button(
+                Scaffold (
+                    topBar = {
+                        AppbarNormalM3(
+                            title = "测试".toStringItemDto(),
+                        )
+                    },
+                ) { paddingValues ->
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight()
+                            .fillMaxSize()
+                            .padding(
+                                top = paddingValues.calculateTopPadding(),
+                            )
+                            .padding(horizontal = APP_PADDING_NORMAL.dp, vertical = 0.dp)
                             .nothing(),
-                        onClick = {
-                            Router
-                                .with(context = this@MainActivity)
-                                .hostAndPath(hostAndPath = AppRouterConfig.APP_TEST1)
-                                .addIntentFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                .forward()
-                        },
-                    ) {
-                        Text(
-                            text = "点我"
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) //
+                    {
+                        AppHeightSpace()
+                        AppHeightSpace()
+                        AppHeightSpace()
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                                .nothing(),
+                            onClick = {
+                                Router
+                                    .with(context = this@MainActivity)
+                                    .hostAndPath(hostAndPath = AppRouterConfig.APP_TEST1)
+                                    .addIntentFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    .forward()
+                            },
+                        ) {
+                            Text(
+                                text = "点我"
+                            )
+                        }
+                        AppHeightSpace()
+
+                        val composition by rememberLottieComposition(
+                            LottieCompositionSpec.RawRes(com.xiaojinzi.lib.res.R.raw.res_empty1)
+                        )
+                        LottieAnimation(
+                            modifier = Modifier
+                                .size(size = 120.dp)
+                                .nothing(),
+                            composition = composition,
+                            iterations = LottieConstants.IterateForever,
                         )
                     }
-                    AppHeightSpace()
-
-                    val composition by rememberLottieComposition(
-                        LottieCompositionSpec.RawRes(com.xiaojinzi.lib.res.R.raw.res_empty1)
-                    )
-                    LottieAnimation(
-                        modifier = Modifier
-                            .size(size = 120.dp)
-                            .nothing(),
-                        composition = composition,
-                        iterations = LottieConstants.IterateForever,
-                    )
                 }
             }
         }
